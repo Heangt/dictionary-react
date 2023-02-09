340be0fa2d442e583001cef0f72a976702686e69
Heang <heangaraya@gmail.com>
2023-02-09 16:42:48 +0100

    Remove the word Example and Definition

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,19 @@
 import React from "react";
 export default function Meanings(props){
    let definitions = props.meanings.definitions;
+   
     return <div>
         <h5>{props.meanings.partOfSpeech}</h5>
         {definitions.map(function(definition, index){
+          
        return <div key={index}> 
        <span>
-        <p>Definition: {definition.definition}</p>
-        <p>Example: {""}<em>{definition.example}</em></p>
+        <p>🔑{definition.definition}</p>
+        <p><em>{definition.example}</em></p>
         </span>
         </div>
+          
         })}
+      
       </div>
       }
