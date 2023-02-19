27d257177c013a9681a5d4a3e775aa902c76df98
Heang <heangaraya@gmail.com>
2023-02-19 18:01:19 +0100

    Style the Phonetics component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,8 +6,16 @@ export default function Phonetics(props) {
   if (audio) {
     return (
       <div>
-        <ReactAudioPlayer src={audio} autoPlay controls />
-        {""} <small>{props.phonetics.text}</small>
+        <ReactAudioPlayer
+          src={audio}
+          autoPlay
+          controls
+          style={{ width: "100px", padding: "10px 0", margin: "-10px 0" }}
+        />
+
+        <span style={{ position: "absolute", padding: "4px 10px" }}>
+          {props.phonetics.text}
+        </span>
       </div>
     );
   } else return null;
