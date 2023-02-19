270cb9b50acfdfd8523af3b61411ebe3ad2ef227
Heang <heangaraya@gmail.com>
2023-02-19 18:41:25 +0100

    Change the default keyword

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Dictionary defaultKeyword="people" />
+      <Dictionary defaultKeyword="celebrate" />
       <span>
         <p>
           <a
